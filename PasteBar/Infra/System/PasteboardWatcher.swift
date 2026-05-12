b9b29f0a7dd106a2b95293f81655e064db321c1c
Freewind <nowindlee@gmail.com>
2026-05-12 23:43:04 +0800

    fix: remove pasteboard watcher sendable warnings
    收口编译 warning：
    - PasteboardWatcher.swift 存在 self 与 onChange 在 @Sendable closure 中被捕获的 warning
    - 编译时有这些 warning，需要消除

--- PasteBar/Infra/System/PasteboardWatcher.swift
+++ PasteBar/Infra/System/PasteboardWatcher.swift
@@ -5,6 +5,7 @@ final class PasteboardWatcher {
   private let pasteboard: NSPasteboard
   private var timer: Timer?
   private var lastChangeCount: Int
+  private var onChange: (() -> Void)?
 
   init(pasteboard: NSPasteboard = .general) {
     self.pasteboard = pasteboard
@@ -13,22 +14,30 @@ final class PasteboardWatcher {
 
   func start(interval: TimeInterval = 0.35, onChange: @escaping () -> Void) {
     stop()
-    timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
-      guard let self else {
-        return
-      }
-      let changeCount = pasteboard.changeCount
-      guard changeCount != lastChangeCount else {
-        return
-      }
-      lastChangeCount = changeCount
-      onChange()
-    }
+    self.onChange = onChange
+    timer = Timer.scheduledTimer(
+      timeInterval: interval,
+      target: self,
+      selector: #selector(handleTimerTick),
+      userInfo: nil,
+      repeats: true
+    )
     RunLoop.main.add(timer!, forMode: .common)
   }
 
   func stop() {
     timer?.invalidate()
     timer = nil
+    onChange = nil
+  }
+
+  @objc
+  private func handleTimerTick() {
+    let changeCount = pasteboard.changeCount
+    guard changeCount != lastChangeCount else {
+      return
+    }
+    lastChangeCount = changeCount
+    onChange?()
   }
 }
